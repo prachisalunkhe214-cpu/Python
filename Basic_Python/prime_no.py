3ce3d45f8320e67baa0d1be4feed65362a4cdda8
prachisalunkhe214 <prachisalunkhe214@gmail.com>
2026-08-25 21:53:42 +0530

    Prime_no

---
+++ Basic_Python/prime_no.py
@@ -0,0 +1,13 @@
+lower = int(input("Enter the lower limit: "))
+upper = int(input("Enter the upper limit: "))
+count = 0
+print("Prime numbers between", lower, "and", upper, "are:")
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
+            count += 1
+print("Total number of prime numbers:", count)
